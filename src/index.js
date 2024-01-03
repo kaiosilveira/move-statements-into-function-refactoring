06e8eb3ad695ff09c1c5018ad0f676eabd8f5548
Kaio Silveira <silveira.kaio@icloud.com>
2024-01-03 15:28:27 +0000

    rename `zznew` to `emitPhotoData`

--- src/index.js
+++ src/index.js
@@ -2,22 +2,22 @@ export function renderPerson(_outStream, aPerson) {
   const result = [];
   result.push(`<p>${aPerson.name}</p>`);
   result.push(renderPhoto(aPerson.photo));
-  result.push(zznew(aPerson.photo));
+  result.push(emitPhotoData(aPerson.photo));
   return result.join('\n');
 }
 
 export function photoDiv(p) {
-  return ['<div>', zznew(p), '</div>'].join('\n');
+  return ['<div>', emitPhotoData(p), '</div>'].join('\n');
 }
 
 function renderPhoto(_aPhoto) {
   return `<p>🌃</p>`;
 }
 
-function zznew(p) {
+function emitPhotoData(aPhoto) {
   return [
-    `<p>title: ${p.title}</p>`,
-    `<p>location: ${p.location}</p>`,
-    `<p>date: ${p.date.toDateString()}</p>`,
+    `<p>title: ${aPhoto.title}</p>`,
+    `<p>location: ${aPhoto.location}</p>`,
+    `<p>date: ${aPhoto.date.toDateString()}</p>`,
   ].join('\n');
 }
